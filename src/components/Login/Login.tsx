4e8916b932d751728efd2e26e043c7324ba95c08
A-lkhater <ahmed.magdy90890@gmail.com>
2026-09-05 04:12:21 +0300

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,94 +1,93 @@
-// import React from 'react'
-
-// export default function Login() {
-//   return (
-//     <div className='login-container bg-warning vh-100'>
-//       <div className="row">
-//         <div className="Users col-md-4  ">
-
-//           <div className="text-center">
-//             <h4>User Management System</h4>
-//             <h6>Sign In</h6>
-//             <span>Enter your credentials to access your account</span>
-//           </div>
-
-//           <form>
-//             <div className="mb-3">
-//               <label >Email </label>
-//               <input type="email" ></input>
-//             </div>
-//             <div className="mb-3">
-//               <label>Password</label>
-//               <input type="password" ></input>
-//             </div>
-//             <button className='btn btn-warning justify-content-center aline-item-center' type='submit'>SIGN IN</button>
-
-//           </form>
-//         </div>
-//       </div>
-
-//     </div>
-//   )
-// }
-
-import axios from 'axios';
-import  { useContext } from 'react'
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import axios from "axios";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import { AuthContext } from "../../context/AuthContext";
-export default function Login() {
-  let{saveUSerData}=useContext(AuthContext)
 
-  let{ register, handleSubmit, formState: { errors }, } = useForm();
-  let navigate=useNavigate();
+export default function Profile() {
+  let { userData } = useContext(AuthContext)
 
-
-  let onSubmit = async(data) => {
-    try {
-
-      let response = await axios.post( 'https://dummyjson.com/auth/login', data)
-      localStorage.setItem("usertoken",response.data.accessToken);
-      saveUSerData();
-      navigate("/dashboard");
-      toast.success("welcom login success")
-    } catch (error) {
-      console.log(error)
-
-    }
-
-  }
+  let {
+    register,
+    formState: { errors },
+  } = useForm();
+  console.log(userData);
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-warning">
-      <div className="text-center bg-white rounded-5  p-5">
-
-        <h4>User Management System</h4>
-        <h6>Sign In</h6>
-        <span>Enter your credentials to access your account</span>
-
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-3">
-            <label>Email</label>
-            <input type="emaill" {...register("username", { required: "email is required" })} placeholder='Enter your email' className="form-control" />
-          </div>
-          {errors?.username && <span className='text-danger'>{errors.username.message}</span>}
-
-          <div className="mb-3">
-            <label>Password</label>
-            <input type="password" {...register("password", { required: "password is required" })} placeholder='Enter your password' className="form-control" />
-
-          </div>
-          {errors?.password && <span className='text-danger'>{errors.password.message}</span>}
-
-          <button
-            className="btn btn-warning w-100"
-            type="submit"
-          >
-            SIGN IN
-          </button>
-        </form>
-
+    <>
+    <div className="title d-flex justify-content-between p-3">
+        <h3>Profile</h3>
       </div>
-    </div>
-  )
+      <hr />
+      <div className="text-center mb-4">
+        <img
+          src={userData?.image}
+          alt="profile"
+          className="rounded-circle"
+          width="120"
+        />
+      </div>
+      <form className="shadow-sm p-4 m-4">
+        <div className="row p-4 m-4">
+          <div className="col-md-6 ">
+            <label>First Name</label>
+            <input
+              className="form-control"
+              value={userData?.firstName}
+              {...register("fristName", { required: "frist Name is required" })}
+            />
+            {errors.fristName && <span className="text-danger">{errors.fristName.message as string}</span>}
+          </div>
+          <div className="col-md-6">
+            <label>Last Name</label>
+            <input
+              className="form-control"
+              value={userData?.lastName}
+
+              {...register("lastName", { required: "Last Name is required" })}
+            />
+            {errors.lastName && <span className="text-danger">{errors.lastName.message as string}</span>}
+          </div>
+        </div>
+        <div className="row p-4 m-4">
+          <div className="col-md-6">
+            <label>Email</label>
+            <input
+              className="form-control"
+              value={userData?.email}
+              {...register("Email", { required: "Email is required" })}
+            />
+            {errors.Email && <span className="text-danger">{errors.Email.message as string}</span>}
+          </div>
+          <div className="col-md-6">
+            <label>Gender</label>
+            <input
+              className="form-control"
+              value={userData?.gender}
+              {...register("Age", { required: "Age is required" })}
+            />
+            {errors.Age && <span className="text-danger">{errors.Age.message as string}</span>}
+          </div>
+        </div>
+        <div className="row p-4 m-4">
+          <div className="col-md-6">
+            <label>Phone Number</label>
+            <input
+              className="form-control"
+              value={userData?.iat}
+              {...register("PhoneNumber", { required: "Phone Number is required" })}
+            />
+            {errors.PhoneNumber && <span className="text-danger">{errors.PhoneNumber.message as string}</span>}
+          </div>
+          <div className="col-md-6">
+            <label>Iat</label>
+            <input
+              className="form-control"
+              value={userData?.iat}
+              {...register("BirthDate", { required: "Birth Date is required" })}
+
+            />
+            {errors.BirthDate && <span className="text-danger">{errors.BirthDate.message as string}</span>}
+          </div>
+        </div>
+      </form>
+    </>)
 }
